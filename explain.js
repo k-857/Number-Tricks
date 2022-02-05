01fd3b1592bf3b9ab650dd88898b13dc8d02e6d4
k-857 <60834543+k-857@users.noreply.github.com>
2022-02-05 13:24:10 -0500

    Add files via upload

---
+++ explain.js
@@ -0,0 +1,17 @@
+document.write("<h1>Number Tricks</h1>");
+document.write("<ul> <li>I added 9 to " + startingNumber + ". " +
+    "The new number is " + newNumber + ". <br> </li>" +
+    "<li>I multipled " + newNumber + " by 2. " + "The new number is " + thirdNumber + ".<br> </li>" +
+
+    "<li>I subtracted 4 from " + thirdNumber + ". " + "The new number is " + fourthNumber +". <br> </li>" +
+
+    "<li>I divided " + fourthNumber + " by two. " + " The new number is " + fifthNumber + ". <br></li>" +
+
+    "<li>I subtracted your original number of " + startingNumber + " from " + fifthNumber + ".<br> " + "<br></li></ul>" +
+
+    "<b> Your final number is " + sixthNumber + "! </b> <br><br>");
+
+"<br><br>" ;
+
+
+alert("Final number is " + sixthNumber + "!");
---
+++ number.js
@@ -0,0 +1,13 @@
+
+		var entry;
+		entry = prompt("Enter a whole number");			
+		entry = parseInt(entry);					
+		var startingNumber = entry;
+								
+
+		newNumber = startingNumber + 9;
+		thirdNumber = newNumber*2;
+		fourthNumber = thirdNumber-4;
+		fifthNumber = fourthNumber/2;
+		sixthNumber = fifthNumber-startingNumber;
+		finalAnswer = sixthNumber;
